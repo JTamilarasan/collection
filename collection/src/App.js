1d083d3d859a4f3dcdbda4585e79f82ad84cd205
JTamilarasan <tamilajv10cse@gmail.com>
2023-09-13 21:12:54 +0530

    changes

--- collection/src/App.js
+++ collection/src/App.js
@@ -1,41 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState('')
-
+  const [addAmount, setAddAmount] = useState();
+  const [subtractAmount, setSubtractAmount] = useState();
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [disable, setdisable] = useState(true);
 
   useEffect(() => {
-localStorage.setItem('MYITEM',"TAMILA2")
-
-
-}, []);
-  useEffect(() => {
-    const sggs=localStorage.getItem('MYITEM',"TAMILA2")
-    if(sggs){
-      setData(sggs)
+    const storedAmount = localStorage.getItem('totalAmount');
+    if (storedAmount) {
+      setTotalAmount(parseInt(storedAmount));
     }
+  }, []);
+  const handleAddition = () => {
+    let newTotal;
+    if(addAmount!=="" && subtractAmount!==""){
+      setdisable(true)
 
+    }
+    else{
+      setdisable(false);
+      if(addAmount!==""){
+         newTotal = totalAmount + addAmount;
 
-}, []);
+      }
+      else{
+        newTotal = totalAmount - subtractAmount;
+      }
 
+    setTotalAmount(newTotal);
+    localStorage.setItem('totalAmount', newTotal.toString());
+    }
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          {data}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{color: "white",fontSize:"42px",lineHeight:"57px", 
+}}>ACC</h1>
+        <b  style={{color: "white",fontSize:"30px",lineHeight:"57px"}} >Collection List</b>
+
+        <nav>
+        <ul>
+            <li><a href="https://www.w3schools.com">Amount List</a></li>
+            <li><a href="https://www.w3schools.com">Add Guys Amount</a></li>
+        </ul>
+</nav>
+<div className="card">
+  <div className="totalname">
+    Total Amount: {totalAmount}
+  </div>
+  <div className="addamount">
+    Add Amount:<br/>&nbsp;<label>
+      <input class="search-text" type="number"
+           value={addAmount}
+           onChange={(e) => setAddAmount(parseInt(e.target.value))}/>
+    </label>
+  </div>
+  <div className="addamount">
+    Sub Amount:<br/>&nbsp;<label>
+      <input class="search-text" value={subtractAmount}
+            onChange={(e) => setSubtractAmount(parseInt(e.target.value))}/>
+    </label>
+  </div>
+  <button  type="button" disabled={disable} onClick={handleAddition}>Submit</button>
+
+
+
+    </div>
+
+        <div className="footer">
+            <p> Developed <br/>by</p>
+            J.Tamilarasan
+        </div>
       </header>
     </div>
   );
